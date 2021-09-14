fff6c0dc3f60da20aa87f41cfa0b2113f4ad7100
seonyoung <4tjsdud2@naver.com>
2021-09-15 01:33:37 +0900

    Start_tabbar

---
+++ Atti/SNSViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SNSViewController.swift
+//  Atti
+//
+//  Created by 장선영 on 2021/09/15.
+//
+
+import UIKit
+
+class SNSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
