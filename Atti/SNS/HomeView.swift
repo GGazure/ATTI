b52236eb831acb738aaa61866b48d455e412f2c4
doridoricomgong <dortm951753@gmail.com>
2021-10-05 00:34:41 +0900

    snsview

---
+++ Atti/SNS/HomeView.swift
@@ -0,0 +1,16 @@
+//
+//  HomeView.swift
+//  Atti
+//
+//  Created by 박승렬 on 2021/10/05.
+//
+import SwiftUI
+
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+                
+            }
+    }
+}
